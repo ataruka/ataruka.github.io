d94750a1bf987a693149ee2fcfff376f9da68f12
ataruka <ataruka203.222@gmail.com>
2024-02-05 21:17:23 +0900

    _

---
+++ spn/script.js
@@ -0,0 +1,19 @@
+function createLines() {
+    let line = document.createElement("div");
+    line.setAttribute("class","line");
+    document.body.appendChild(line);
+
+    line.style.left = Math.random() * innerWidth + "px";
+    line.style.animationDuration = 6 + Math.random() * 24 + "s";
+    line.style.width = Math.random() * 12 + "px";
+    line.style.height = Math.random() * 12 + "px";
+
+    setTimeout(function () {
+        document.body.removeChild(line);
+    }, 24000)
+}
+
+setInterval(function(){
+    createLines();
+
+}, 100)
